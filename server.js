3a0252874d9bcd78358bca823b4ea5d2ba10c1b2
Harry Leonardo <harryleonardo@MBP-Harry.lan>
2018-12-09 00:37:34 +0700

    Add project page

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/about',(req,res) => {
     });
 });
 
+app.get('/projects',(req,res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 // Send Back JSon with errorMessage
 app.get('/bad', (req, res) => {
     res.send({
